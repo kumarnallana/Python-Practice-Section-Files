c6d6f2bbd1e9a0dcc119daa45921688f8fccec7b
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-16 09:44:21 +0530

    chore: configure language server and IDE settings for multi-folder practice workspace

--- python_advance/Asyncio_Project_2/src/services.py
+++ python_advance/Asyncio_Project_2/src/services.py
@@ -1,1 +1,25 @@
 import httpx
+import json
+from pathlib import Path
+
+
+class ExternalApiCall:
+    async def fetch(self, client: httpx.AsyncClient, url: str):
+        response = await client.get(url)
+
+        response.raise_for_status()
+
+        return response.json()
+
+    async def get(self):
+        async with httpx.AsyncClient as client:
+
+            external_db = await self.fetch(client, "https://dummyjson.com/products")
+
+            return external_db
+
+    def create_local_db(self):
+
+        external_db = self.get()
+
+        base_dir =
