c6a0068243da79b04fb11a38f908b9b76aa01ee2
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-20 09:23:38 +0530

    Explain set operations and iterable requirements in Python

---
+++ DSA_WTH_PYTHON/basic_level/26-Remove-Duplicates.py
@@ -0,0 +1,33 @@
+nums: list[int] = [0, 0, 1, 1]
+
+
+class Solution:
+
+    def manual_length(self, nums: list[int]) -> int:
+        count: int = 0
+        for _ in nums:
+            count += 1
+
+        return count
+
+    def removeDuplicates(self, nums: list[int]) -> int:
+        n = self.manual_length(nums)
+        slow = 0
+        fast = 0
+        seen = set()
+
+        while fast < n:
+
+            if nums[fast] not in seen:
+                nums[slow] = nums[fast]
+                slow += 1
+                seen += nums[fast]
+
+            fast += 1
+
+        return slow
+
+
+solution = Solution()
+
+print(solution.removeDuplicates(nums))
