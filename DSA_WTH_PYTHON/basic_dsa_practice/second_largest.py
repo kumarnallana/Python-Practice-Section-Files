c475363501e888b86fc1ab192d9df13d7d17e710
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-25 09:46:03 +0530

    Solve a problem to find the second largest number in python

---
+++ DSA_WTH_PYTHON/basic_dsa_practice/second_largest.py
@@ -0,0 +1,19 @@
+nums = [10, 5, 20, 15]
+
+
+def second_largest(nums):
+    largest = nums[0]
+    second = nums[0]
+
+    for num in nums:
+        if num > largest:
+            second = largest
+            largest = num
+
+        elif num > second and num < largest:
+            second = num
+
+    return second
+
+
+print(second_largest(nums))
