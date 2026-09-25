fe3f391abdc400fddf3e5c81b4f5bc1c42053933
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-25 15:39:00 +0530

    Solved 387 leet code problem

--- DSA_WTH_PYTHON/basic_level/387_frst_unique_char.py
+++ DSA_WTH_PYTHON/basic_level/387_frst_unique_char.py
@@ -1,20 +1,4 @@
-# def first_non_repeating_char(text):
-#     frequency = {}
-
-#     for char in text:
-#         if char in frequency:
-#             frequency[char] = frequency[char] + 1
-#         else:
-#             frequency[char] = 1
-
-#     for char in text:
-#         if frequency[char] == 1:
-#             return char
-
-#     return None
-
-
-# print(first_non_repeating_char("swiss"))
+# LeetCode #387: First Unique Character in a String. It asks for the first non-repeating character and returns its index
 
 input: str = "Leetcode"
 
