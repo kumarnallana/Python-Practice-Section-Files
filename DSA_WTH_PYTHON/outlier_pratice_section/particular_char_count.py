cebabb62310310543d4e2d3be299370654bf2cd4
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-26 16:38:34 +0530

    fix: safely handle empty strings in character counter

--- DSA_WTH_PYTHON/outlier_pratice_section/particular_char_count.py
+++ DSA_WTH_PYTHON/outlier_pratice_section/particular_char_count.py
@@ -1,21 +1,17 @@
-
-words: list[str] = ["apple", "Arora" "banana", "ant", "cat", "air"]
-
-
-def char_count(words: list[str]) -> int | str:
-    count = 0
+def char_count(words: list[str]) -> int:
+    count: int = 0
     letter: str = "a"
 
     for word in words:
-        try:
-            if not word:
-                raise KeyError("Words list should not be empty")
-            if word[0].lower() == letter.lower():
-                count += 1
-        except Exception as e:
-            return f"Error: {e}"
+        if not word:
+            continue
+
+        if word[0].lower() == letter:
+            count += 1
 
     return count
 
+
+words = ["Apple", "", "banana", "ant", "Air", "cat"]
 
 print(char_count(words))
